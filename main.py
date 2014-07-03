06b0bb63fd8d097a09773ea6d50a204169d34e64
Paola Falcon <cacoepy@gmail.com>
2014-07-03 14:35:40 -0500

    que es esto

---
+++ main.py
@@ -0,0 +1,14 @@
+__author__ = 'Paola'
+import old_stuff
+class Clima:
+
+
+class SeresVivos:
+    def __init__(self):
+        self.Vitalidad = 100
+        self.Edad = 0
+        self.Fertilidad = True
+        self.Energia = 100
+
+class Plantas(SeresVivos):
+
